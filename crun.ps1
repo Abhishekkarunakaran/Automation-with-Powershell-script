cf55834ae08e1f2b1e66a07bf0fb91932e6267b5
Abhishekkarunakaran <abhishekkarunakaran100@gmail.com>
2021-03-12 13:28:50 +0530

    Replaced cd to Set-Location

--- crun.ps1
+++ crun.ps1
@@ -2,52 +2,44 @@
 
 $input = $args[0]
 
-if($input -eq $null)
-{
-    	Write-Host "Enter the file name as arguement"
-    	exit
+if ($input -eq $null) {
+	Write-Host "Enter the file name as arguement"
+	exit
 }
 
-if(($input -like "*.cpp") -or ($input -like "*.c"))    	
-{        	
-	if(Test-Path a.exe)
-       	{
-       		Remove-Item a.exe
-       	}
-       	g++ $input 	#using g++ for compiling both c and c++ programs
-       	if(Test-Path a.exe)
-       	{
-       		./a.exe
-        }
-        else
-       	{
-            	exit
-        }
+if (($input -like "*.cpp") -or ($input -like "*.c")) {        	
+	if (Test-Path a.exe) {
+		Remove-Item a.exe
+	}
+	g++ $input 	#using g++ for compiling both c and c++ programs
+	if (Test-Path a.exe) {
+		./a.exe
+	}
+	else {
+		exit
+	}
 }
-elseif($input -like "*.java")
-{
+elseif ($input -like "*.java") {
 	$name_array = $input.Split(".")
 	$class_name = $name_array[0]
 	$dir_name = "_crun_java_$class_name"
  		
-	if(Test-Path $dir_name)
-	{
+	if (Test-Path $dir_name) {
 		Remove-Item $dir_name -Recurse
 	}
 		
 	mkdir $dir_name > $null
 	Copy-Item $input $dir_name
-	cd $dir_name
+	Set-Location $dir_name
 	
 	javac $input
 
 	java $class_name
 
-	cd ..
+	Set-Location ..
 	exit
 }
-else
-{
+else {
 	Write-Host "Invalid filename with extension"
 	exit
 }	
